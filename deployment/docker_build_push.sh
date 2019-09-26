5771a63bae297a8822730b4163cfd957dbc1814a
Fred <fmahiant@gmail.com>
2019-09-26 09:03:30 +0200

    Update docker_build_push.sh

--- deployment/docker_build_push.sh
+++ deployment/docker_build_push.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 mvn help:evaluate -N -Dexpression=project.version | grep -v '\['
-project_version=$(mvn help:evaluate -N -Dexpression=project.version | grep -v '\[')
+project_version="$(mvn help:evaluate -N -Dexpression=project.version | grep -v '\[')"
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker build --build-arg jar_revision=${project_version} --tag raspinloop/modelicamodel-service .
+docker build --build-arg "jar_revision=${project_version}" --tag raspinloop/modelicamodel-service .
 docker tag raspinloop/modelicamodel-service:latest raspinloop/modelicamodel-service:${project_version}
 docker push raspinloop/modelicamodel-service:${project_version}
